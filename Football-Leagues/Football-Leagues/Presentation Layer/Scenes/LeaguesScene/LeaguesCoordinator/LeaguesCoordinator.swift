b711591cf2c6452edd342d53ba055c4598dfff8b
Amin <mahmoudamin373@gmail.com>
2023-10-16 22:43:56 +0300

    arch : define the architecture
    * add architecture
    * add starting point from scene delegate

---
+++ Football-Leagues/Football-Leagues/Presentation Layer/Scenes/LeaguesScene/LeaguesCoordinator/LeaguesCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  LeaguesCoordinator.swift
+//  Football-Leagues
+//
+//  Created by Amin on 16/10/2023.
+//
+
+import UIKit
+
+protocol LeaguesCoordinating: Coordinating{
+    
+}
+struct LeaguesCoordinator:LeaguesCoordinating{
+    
+    func start() {
+        
+    }
+}
--- Football-Leagues/Football-Leagues/Presentation Layer/Scenes/LeaguesScene/LeaguesView/LeaguesViewController.swift
+++ Football-Leagues/Football-Leagues/Presentation Layer/Scenes/LeaguesScene/LeaguesView/LeaguesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LeaguesViewController.swift
 //  Football-Leagues
 //
 //  Created by Amin on 16/10/2023.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LeaguesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
